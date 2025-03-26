03d6eec7272e49436b5a3e36218d2cbba0526b20
Yash Pathak <106083687+Yash7776@users.noreply.github.com>
2025-03-26 12:12:01 +0000

    WebSite Not Redaring Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-import React from 'react'
-import './App.css'
-import '../src/index.css'
-import Navbar from './components/navbar'
-import Home from './components/Home'
-import Login from './components/Login'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from 'react-router-dom';
-import Products from './components/Products'
-import Productview from './components/Productview'
-import Footer from './components/Footer'
+import React from "react";
+import "./App.css";
+import "../src/index.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Products from "./components/Products";
+import Productview from "./components/Productview";
+import Footer from "./components/Footer";
 function App() {
-
   return (
     <>
       <Router>
         <Navbar />
         <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/login' element={<Login />} />
-          <Route exact path='/products' element={<Products />} />
-          <Route exact path='/productview' element={<Productview />} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/products" element={<Products />} />
+          <Route exact path="/productview" element={<Productview />} />
         </Routes>
         <Footer />
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
